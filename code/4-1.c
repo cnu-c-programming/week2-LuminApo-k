f960f9043aba520d30495e6d14a9ecd2d3adf8ad
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:50:45 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -4,7 +4,7 @@ int main() {
     int a, b;
     char op;
 
-    scanf("%d %c %d", &a, &op, &b);
+    scanf("%d %d %c", &a, &b, &op);
 
     if (op == '+') printf("%d\n", a + b);
     else if (op == '-') printf("%d\n", a - b);
