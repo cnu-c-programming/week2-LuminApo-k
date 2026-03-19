bcd3ad924ece96d68a72abe3cf8e2e23219b419a
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:23:14 +0900

    Implement basic calculator functionality

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %c %d", &a, &op, &b);
+
+    if (op == '+') printf("%d\n", a + b);
+    else if (op == '-') printf("%d\n", a - b);
+    else if (op == '*') printf("%d\n", a * b);
+    else if (op == '/') printf("%d\n", a / b);
 
     return 0;
 }
-
