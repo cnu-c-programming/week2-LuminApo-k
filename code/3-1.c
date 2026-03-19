f8097f8c02f67b9f13acf03d3a4e10d79ea2ba8b
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:38:53 +0900

    Update 3-1.c

--- code/3-1.c
+++ code/3-1.c
@@ -4,7 +4,7 @@ int main() {
     char a = 'a';
     char b = 'b';
 
-    printf("sizeof(a + b) is: %lu\n", sizeof(a + b));
+    printf("sizeof(a + b) is : %lu\n", sizeof(a + b));
 
     return 0;
 }
