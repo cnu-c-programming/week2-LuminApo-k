9aec44279458cad4346904caf8145bbcec74dc9a
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:46:40 +0900

    Update 4-2.c

--- code/4-2.c
+++ code/4-2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 2; i <= 9; i++) {
+    for (int i = 1; i <= 9; i++) {
         for (int j = 1; j <= 9; j++) {
             printf("%d x %d = %d\n", i, j, i*j);
         }
