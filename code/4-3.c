faf87ddf73dd30360bc9c465c99c936ecbd809bd
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:23:34 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    int n;
+    int isPrime = 1;
+
+    scanf("%d", &n);
+
+    if (n <= 1) isPrime = 0;
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            isPrime = 0;
+            break;
+        }
+    }
+
+    if (isPrime) printf("true\n");
+    else printf("false\n");
 
     return 0;
 }
-
