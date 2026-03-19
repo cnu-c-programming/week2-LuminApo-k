80292f19162ed9dfdfdbc88b46a7fd8a8802cf2f
LuminApo-k <beomseomoon@gmail.com>
2026-03-19 22:20:31 +0900

    Update 2-4.c

--- code/2-4.c
+++ code/2-4.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
+    float a = 0.1 + 0.2;
+    float b = 0.3;
+
+    printf("a = %.10f\n", a);
+    printf("b = %.10f\n", b);
+    printf("a == b is %d\n", a == b);
+
+    double c = 0.1 + 0.2;
+    double d = 0.3;
+
+    printf("c = %.10lf\n", c);
+    printf("d = %.10lf\n", d);
+    printf("c == d is %d\n", c == d);
 
     return 0;
 }
-
